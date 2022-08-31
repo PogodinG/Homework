7123897df34a6bbb5b7d629d6d86aae653465de7
Gleb <glebpogodin96@mail.ru>
2022-08-31 14:03:48 +0500

    Задача 4

---
+++ Задача 2/Program.cs
@@ -0,0 +1,10 @@
+﻿double a = 44, b = 5, c = 78;
+            
+                if (a > b && a > c)
+                    Console.WriteLine("Nubmer Max = " + a); 
+                else if (b > c)
+                    Console.WriteLine("Number Max = " + b); 
+                else
+                    Console.WriteLine("Number Max = " + c); 
+            
+            Console.ReadLine();
